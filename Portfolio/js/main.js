6f6899743678c299f1d20643323e03ee8a20d752
Bastien Lhuaire <bastien.lhuaire@outlook.fr>
2017-01-25 15:56:31 +0100

    transfere du projet sur github

---
+++ Portfolio/js/main.js
@@ -0,0 +1,70 @@
+$( document ).ready(function() {
+	//Droppable Chunk
+	$( ".draggable_area .circle").each(function(){
+		$( this ).droppable({
+			hoverClass: "circle-hover",
+			drop: function( event, ui ) {
+
+		    	$(this).css({
+		    		'z-index' : 3,
+		    		'height': '500',
+		    		'width': '500',
+		    		'-webkit-transform': 'scale( 10 )',
+		    		'-moz-transform': 'scale( 10 )',
+		   			'-o-transform': 'scale( 10 )',
+		    		'-ms-transform': 'scale( 10 )',
+		   			'transform': 'scale( 10 )'
+ 		    	});
+ 		    	$("#pawn").css("display","none");
+ 		    	//replace the pawn to initial position
+ 		    	$("#pawn").draggable({revert: "valid"});
+ 		    	$(".partie_"+$(this).attr("name")).fadeIn(2000);
+		   		$(".partie_"+$(this).attr("name")).find(".back").attr("name",$(this).attr("name"));
+		    },
+		    revert: true
+		});
+	});
+
+	//Pawn Draggable
+	$("#pawn").draggable({
+		refreshPositions: true,
+		revert: "invalid",
+		cursor: "grabbing"
+	});
+
+	//Back Button
+	$(".back").on("click",function(){
+		$(".circle_"+$(this).attr("name")).css({
+			'height': '70',
+			'width': '70',
+			'-webkit-transform': 'scale( 1 )',
+			'-moz-transform': 'scale( 1 )',
+			'-o-transform': 'scale( 1 )',
+			'-ms-transform': 'scale( 1 )',
+			'transform': 'scale( 1 )',
+			'z-index' : 1
+ 		});
+ 		$("#pawn").draggable({revert: "invalid"});
+ 		$(".partie_"+$(this).attr("name")).fadeOut(1000);
+ 		$("#pawn").fadeIn(2000);
+ 		$(".circle_"+$(this).attr("name")).css({
+ 			'-webkit-transform': "",
+			'-moz-transform': "",
+			'-o-transform': "",
+			'-ms-transform': "",
+			'transform': ""
+ 		});
+	});
+
+	//images projets
+	$(".projet_div").on("click","img",function(){
+		$(this).css({
+			'z-index' : 3,
+			'-webkit-transform': 'scale( 2 )',
+			'-moz-transform': 'scale( 2 )',
+			'-o-transform': 'scale( 2 )',
+			'-ms-transform': 'scale( 2 )',
+			'transform': 'scale( 2 )'
+		});
+	}); 
+});
